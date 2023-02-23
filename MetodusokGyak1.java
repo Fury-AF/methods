723f4c3459758aee8bb2f73cceb78d6badd5d41d
Fury-AF <99542261+Fury-AF@users.noreply.github.com>
2023-02-23 17:39:27 +0100

    First commit

---
+++ MetodusokGyak1.java
@@ -0,0 +1,19 @@
+public class MetodusokGyak1 {
+    public static void main(String[] args) {
+
+    }
+    private static void elso10SzamOsszege{
+        int osszeg=0;
+        for (int i =0 ; i<10 ;i++){
+            osszeg+=1;
+        }
+    }
+    private static void kiir{
+        System.out.println("Az első 10 szám összege: "+ osszeg);
+
+    }
+
+    private static void osszead(int a, int b){
+        System.out.println("%d+%d = %/d\n",a,b,a+b);
+    }
+}
