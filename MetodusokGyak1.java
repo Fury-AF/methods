d9ce5fc7210ba1eac1627098224a96ff7f4a9364
Fury-AF <99542261+Fury-AF@users.noreply.github.com>
2023-02-23 17:50:58 +0100

    Javított

--- MetodusokGyak1.java
+++ MetodusokGyak1.java
@@ -2,17 +2,17 @@ public class MetodusokGyak1 {
     public static void main(String[] args) {
 
     }
-    private static void elso10SzamOsszege(){
+    private static int elso10SzamOsszege(){
         int osszeg=0;
         for (int i =0 ; i<10 ;i++){
             osszeg+=1;
         }
+        return osszeg;
     }
-    private static void kiir(){
-        System.out.println("Az első 10 szám összege: "+ elso10SzamOsszege());
+    private static void kiir(String szoveg){
+        System.out.println(szoveg);
     }
-
-    /*private static void osszead(int a, int b){
-        System.out.println("%d+%d = %/d\n",a,b,a+b);
-    }*/
+    private static int osszead(int a, int b){
+      return a+b;
+    }
 }
