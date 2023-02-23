42a1219163825001958fe817a2cbe317e5dd8f35
Fury-AF <99542261+Fury-AF@users.noreply.github.com>
2023-02-23 17:58:02 +0100

    Uj

--- MetodusokGyak1.java
+++ MetodusokGyak1.java
@@ -1,6 +1,6 @@
 public class MetodusokGyak1 {
     public static void main(String[] args) {
-
+        kiir("%d+%d= %d\n"+osszead(3,4)+','+elso10SzamOsszege());
     }
     private static int elso10SzamOsszege(){
         int osszeg=0;
